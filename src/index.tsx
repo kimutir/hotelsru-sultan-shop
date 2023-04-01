ee2f8fe257e8162948aa63d774bb197625de38ae
Timur Kim <kimutir@gmail.com>
2023-04-01 17:26:59 +1100

    json

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./app/app";
 import { store } from "./store/store";
+import { HashRouter } from "react-router-dom";
 
 const div = document.getElementById("root");
 const root = createRoot(div);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </Provider>,
 );
