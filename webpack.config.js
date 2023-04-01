bc97ed4bc4033252b28482c6a9bd98e5110c9748
Timur Kim <kimutir@gmail.com>
2023-04-01 21:35:11 +1100

    prod

--- webpack.config.js
+++ webpack.config.js
@@ -3,70 +3,73 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const url = require("node:url");
 // import url from 'node:url'
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-    alias: {
-      "@assets": path.resolve(__dirname, "./hotelsru-sultan-shop/assets/"),
-      "@custom": path.resolve(__dirname, "./src/app/custom/"),
-      "@pages": path.resolve(__dirname, "./src/app/pages/"),
-      "@layouts": path.resolve(__dirname, "./src/app/layouts/"),
-      "@utils": path.resolve(__dirname, "./src/app/utils/"),
-      "@store": path.resolve(__dirname, "./src/store/"),
-      "@elements": path.resolve(__dirname, "./src/app/elements/"),
-      "@components": path.resolve(__dirname, "./src/app/components/"),
-      "@containers": path.resolve(__dirname, "./src/app/containers/"),
+module.exports = (env, options) => {
+  const mode = options.mode;
+  return {
+    entry: "./src/index.tsx",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+      publicPath: "/",
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "ts-loader",
-        },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"],
+      alias: {
+        "@assets": path.resolve(__dirname, "./hotelsru-sultan-shop/assets/"),
+        "@custom": path.resolve(__dirname, "./src/app/custom/"),
+        "@pages": path.resolve(__dirname, "./src/app/pages/"),
+        "@layouts": path.resolve(__dirname, "./src/app/layouts/"),
+        "@utils": path.resolve(__dirname, "./src/app/utils/"),
+        "@store": path.resolve(__dirname, "./src/store/"),
+        "@elements": path.resolve(__dirname, "./src/app/elements/"),
+        "@components": path.resolve(__dirname, "./src/app/components/"),
+        "@containers": path.resolve(__dirname, "./src/app/containers/"),
       },
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          "@teamsupercell/typings-for-css-modules-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[name]__[local]___[hash:base64:5]",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "ts-loader",
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            "style-loader",
+            "@teamsupercell/typings-for-css-modules-loader",
+            {
+              loader: "css-loader",
+              options: {
+                modules: {
+                  localIdentName: "[name]__[local]___[hash:base64:5]",
+                },
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.png$/,
-        loader: "file-loader",
-        options: {
-          name() {
-            if (process.env.NODE_ENV === "production") {
-              return "hotelsru-sultan-shop/[path][name].[ext]";
-            }
+          ],
+        },
+        {
+          test: /\.png$/,
+          loader: "file-loader",
+          options: {
+            name() {
+              if (mode === "production") {
+                return "hotelsru-sultan-shop/[path][name].[ext]";
+              }
 
-            return "[contenthash].[ext]";
+              return "[contenthash].[ext]";
+            },
           },
         },
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "./dist"),
+      ],
     },
-    historyApiFallback: true,
-  },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "./dist"),
+      },
+      historyApiFallback: true,
+    },
+  };
 };
